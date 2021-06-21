616269d6dc3bd33488df0497c2b4fe97a1f5d685
JUESOP <76995758+JUESOP@users.noreply.github.com>
2021-06-21 12:11:48 +0900

    Add files via upload

---
+++ 이솝/Test1110.java
@@ -0,0 +1,36 @@
+package codingTest;
+
+import java.util.Scanner;
+
+public class Test1110 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int Number = sc.nextInt();
+		
+		int N = Number;
+		int count = 0;
+        
+        while(true) {
+			N = (N % 10 * 10) + (((N / 10) + (N % 10)) % 10);
+			count++;
+			
+			if(N == Number) {
+			break;
+			}
+			
+		}
+			System.out.println(count);
+	}
+	
+}
+		
+//    	do{ N = (N % 10 * 10) + (((N / 10) + (N % 10)) % 10);
+//			count++;
+//	    } while(N != Number);
+		
+//		System.out.println(count);
+//	}
+
+//}
