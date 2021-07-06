d818106f3c805159bead94fc6e449328abd8a300
qkralswl689 <65608960+qkralswl689@users.noreply.github.com>
2021-07-07 00:26:35 +0900

    Update bj17608.java

--- 민지/ct0706/bj17608.java
+++ 민지/ct0706/bj17608.java
@@ -14,27 +14,24 @@ public class bj17608 {
 
         for(int i = 0; i < n; i++ ){
             stack.add(sc.nextInt());
-            System.out.println("stack = " + stack);
+         /*   System.out.println("stack = " + stack);*/
         }
-/*        while (!stack.isEmpty()){
-            int k = stack.pop();
 
-        }*/
         int[] answer = new int[stack.size()];
         int count = 1;
-        for(int i = 0; i < stack.size(); i++){
-            while (!stack.isEmpty()){
-                answer[i] = stack.pop();
-                System.out.println("answer[i] = " + answer[i]);
-            }
-            System.out.println("answer[0] = " + answer[0]);
-            if(answer[0] > answer[i] ){
-                count++;
-            }
-            if(answer[0] < answer[i] && answer[0] == answer[i]){
-                continue;
-            }
+        for(int i = 0; i < n; i++){
+            answer[i] = stack.pop();
+          /*  System.out.println("answer[i] = " + answer[i]);
+*/
+              if(answer[0] < answer[i] ){
+                  count++;
+              }
+              if(answer[0] > answer[i] && answer[0] == answer[i]){
+                  continue;
 
-        } System.out.println("count = " + count);
+          }
+
+        }/* System.out.println("count = " + count);*/
+        System.out.print(count);
     }
 }
