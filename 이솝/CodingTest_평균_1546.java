704d63e116c45da2f1c325ed282488637363d335
JUESOP <76995758+JUESOP@users.noreply.github.com>
2021-07-16 12:12:53 +0900

    Update CodingTest_평균_1546.java

--- 이솝/CodingTest_평균_1546.java
+++ 이솝/CodingTest_평균_1546.java
@@ -7,17 +7,15 @@ public class Code {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
-//		int N = sc.nextInt();
-//		double arr[] = new double[N];
 		double arr[] = new double[sc.nextInt()];
 
-		double max = arr[0];
+		double max = 0;
 		double sum = 0;
 
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
-
+			max = arr[0];
+			
 			if (arr[i] > max) {
 				max = arr[i];
 			}
